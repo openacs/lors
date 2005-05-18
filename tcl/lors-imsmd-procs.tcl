67dd0c293ed8c57ad3d33c80282d772ef86fcba0
Miguel Marin <miguelmmarin@viaro.net>
2005-05-18 17:13:55 +0000

    making changes to fit to previous file version

--- tcl/lors-imsmd-procs.tcl
+++ tcl/lors-imsmd-procs.tcl
@@ -140,7 +140,8 @@ namespace eval lors::imsmd {
                 set prefix [[$tree child all imsmd:record] prefix]
                 set lom [$tree child all $var_lom]
             } elseif { ![empty_string_p [$tree child all adlcp:location]] } {
-                set lom [[dom parse [read [open $dir/[[$tree child all adlcp:location] text]]]] documentElement]
+		dom parse [::tDOM::xmlReadFile $dir/[[$tree child all adlcp:location] text]] doc
+		set lom [$doc documentElement]
                 set prefix [$lom prefix]
             } else {
                 set lom 0
