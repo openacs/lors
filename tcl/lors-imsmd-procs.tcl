c606ddcb1ce018f85e4a84879b369d4bdd3b4e24
Miguel Marin <miguelmmarin@viaro.net>
2005-06-09 17:11:54 +0000

    changing assigment way on variable doc, since was interpreting the result as a command

--- tcl/lors-imsmd-procs.tcl
+++ tcl/lors-imsmd-procs.tcl
@@ -140,7 +140,7 @@ namespace eval lors::imsmd {
                 set prefix [[$tree child all imsmd:record] prefix]
                 set lom [$tree child all $var_lom]
             } elseif { ![empty_string_p [$tree child all adlcp:location]] } {
-		dom parse [::tDOM::xmlReadFile $dir/[[$tree child all adlcp:location] text]] doc
+		set doc [dom parse [::tDOM::xmlReadFile $dir/[[$tree child all adlcp:location] text]]]
 		set lom [$doc documentElement]
                 set prefix [$lom prefix]
             } else {
